8a8140e89e0af4c6b52286a1059838b3e01afced
Naquee <naquee@redrockcapital.in>
2024-12-17 18:01:13 +0530

    task completed

---
+++ my-app/src/app/components/employee-list/employee-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-list',
+  imports: [],
+  template: '<h1 class = "commanText">Admin List Data</h1>',
+  styles: ['.primary{color:red}']
+})
+export class EmployeeListComponent {
+
+}
